6d72ca2a771abe66fff1ffce2e1b776dc7017b4a
wangzhiyuan <wangzhiyuan@playcrab.com>
2018-09-19 19:25:39 +0800

    增加排行榜范围随机

---
+++ src/com/game/core/common/utils/RandomUtil.java
@@ -0,0 +1,51 @@
+package com.game.core.common.utils;
+
+import java.util.Random;
+
+public class RandomUtil {
+	
+	/**
+	 * ÅÅÐÐ°ñ·¶Î§Ëæ»ú
+	 * @param myRank
+	 * @param size
+	 * @param getCount
+	 * @return
+	 */
+	public static int getRandomRank(int myRank,int size,int getCount) {
+		Random random=new Random();
+		if(size<getCount) {
+			int nextInt = random.nextInt(size-1)+1;
+			if(nextInt<myRank) {
+				return nextInt;
+			} else {
+				return nextInt+1;
+			}
+		}
+		int beforeCount=myRank-1;
+		int afterCount=size-myRank;
+		int nextInt = random.nextInt(getCount)+1;
+		if(beforeCount<getCount/2) {
+			if(nextInt<myRank) {
+				return nextInt;
+			} else {
+				return nextInt+1;
+			}
+		}
+		if(afterCount<getCount/2) {
+			int rank= size-nextInt+1;
+			if(rank<=myRank) {
+				return rank-1;
+			} else {
+				return rank;
+			}
+		}
+		int a=getCount/2;
+		if(nextInt<=a) {
+			return myRank+nextInt-a-1;
+		} else {
+			return myRank+nextInt-a;
+		}
+	}
+	
+	
+}
